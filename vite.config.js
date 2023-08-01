80302aa6387533b45d86c1d5c5829318e3c6e147
Hardik Dudeja <hardikdudeja@192.168.1.77>
2023-08-01 23:52:50 +0530

    code added

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // we can define entry point as index.js or any other file
+})
